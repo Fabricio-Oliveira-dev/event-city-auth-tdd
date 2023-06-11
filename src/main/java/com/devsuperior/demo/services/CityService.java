4a7c36b05dfeb5a45284aabc3baac02e20adfa7c
Fabricio Pita <fabricio001400@gmail.com>
2023-06-11 18:07:41 -0300

    City tests passing

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> resultCities = repository.findAll(Sort.by("name"));
+        return resultCities.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+}
